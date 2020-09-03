50b3fe4af150280bfde9fe0419df5578dffe4b3b
Akshay Sankle <32200419+zukros@users.noreply.github.com>
2020-09-03 18:10:02 +0530

    Print & comment statements

--- Akshay.py
+++ Akshay.py
@@ -1,1 +1,17 @@
-print("Hello World 7")
+print("Hello World")  # single line Comment
+
+""""
+Multiline
+Comment
+"""
+
+print("Hello", end="Cluster")  # combining two print
+print("To You")
+
+print("Hello", end=" ")  # combining two print with a space
+print("To You")
+print("")
+
+print("Hello", "To You")
+
+print("I am the \n Best \t")  # implementation of \n & \t
