df4690f9e7842156bbfbc8d74096c7b4ce2e4207
Akshay Sankle <32200419+zukros@users.noreply.github.com>
2020-09-04 18:11:32 +0530

    Input and output

---
+++ variable.py
@@ -0,0 +1,15 @@
+var1 = "Hello"
+var2 = " to Skyler"
+var3 = 36.7
+var4 = 4
+""""
+print(type(var1))
+print(type(var2))
+print(type(var3))
+"""
+print(var1 + var2)
+print(var4 + var3)
+
+print("Enter Your Name")
+input1 = input()
+print("You Entered" , input1)
